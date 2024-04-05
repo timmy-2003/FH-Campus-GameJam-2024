bb9a9cf9ce8abbd010a75b264f7984f139a7f2dd
Moritz Lindner <moritz.lindner@stud.fh-campuswien.ac.at>
2024-04-05 18:25:22 +0200

    spawn balls next to cart

--- Unity Project/Assets/Coding/Golfball/GolfballSpawner.cs
+++ Unity Project/Assets/Coding/Golfball/GolfballSpawner.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GolfballSpawner : MonoBehaviour
 {
 
+    public GameObject golfCart;
     public GameObject golfball;
     public float spawnRate;
     private float time = 0;
@@ -14,7 +15,11 @@ public class GolfballSpawner : MonoBehaviour
         time += Time.deltaTime;
         if (time >= spawnRate)
         {
-            Vector3 spawnPosition = new Vector3(Random.Range(-10f, 10f), 5, Random.Range(-10f, 10f));
+            float cartPositionX = golfCart.transform.position.x;
+            float cartPositionY = golfCart.transform.position.y;
+            float cartPositionZ = golfCart.transform.position.z;
+            Vector3 spawnPosition = new Vector3(Random.Range(cartPositionX - 10f, cartPositionX + 10f),
+                cartPositionY + Random.Range(5f, 10f), Random.Range(cartPositionZ - 10f, cartPositionZ + 10f));
             Instantiate(golfball, spawnPosition, Quaternion.identity);
 
             time = 0;
